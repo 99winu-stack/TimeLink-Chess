affad977f2108f27a26c5015f379d2ec095be84f
Nguyen Le Hoang <nguyen.lehoang@student.htw-berlin.de>
2026-06-22 15:09:09 +0200

    Add Image class

---
+++ Game/GameUI/Images.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using ChessLogic;
+
+namespace GameUI
+{
+    public static class Images
+    {
+        private static readonly Dictionary<PieceType, ImageSource> whiteSources = new()
+        {
+            { PieceType.Pawn, LoadImage("Assets/pawnP.png") },
+            { PieceType.Rook, LoadImage("Assets/rookP.png") },
+            { PieceType.Knight, LoadImage("Assets/knightP.png") },
+            { PieceType.Bishop, LoadImage("Assets/bishopP.png") },
+            { PieceType.Queen, LoadImage("Assets/queenP.png") },
+            { PieceType.King, LoadImage("Assets/kingP.png") },
+        };
+        private static readonly Dictionary<PieceType, ImageSource> blackSources = new()
+        {
+            { PieceType.Pawn, LoadImage("Assets/pawnB.png") },
+            { PieceType.Rook, LoadImage("Assets/rookB.png") },
+            { PieceType.Knight, LoadImage("Assets/knightB.png") },
+            { PieceType.Bishop, LoadImage("Assets/bishopB.png") },
+            { PieceType.Queen, LoadImage("Assets/queenB.png") },
+            { PieceType.King, LoadImage("Assets/kingB.png") },
+        };
+        public static ImageSource LoadImage(string filePath)
+        {
+            return new BitmapImage(new Uri(filePath, UriKind.Relative));
+        }
+
+        public static ImageSource GetImage(Player color, PieceType type)
+        {
+            return color switch
+            {
+                Player.White => whiteSources[type],
+                Player.Black => blackSources[type],
+                _ => null
+            };
+        }
+
+        public static ImageSource GetImage(Piece piece)
+        {
+            if(piece == null)
+            {
+                return null;
+            }
+            return GetImage(piece.Color, piece.Type);
+        }
+    }
+}
--- Game/GameUI/MainWindow.xaml.cs
+++ Game/GameUI/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ChessLogic;
 
 namespace GameUI
 {
@@ -16,9 +17,25 @@ namespace GameUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Image[,] PieceImages = new Image[8, 8];
+
         public MainWindow()
         {
             InitializeComponent();
+            InitilaizeBoard();
+        }
+
+        private void InitilaizeBoard()
+        {
+            for (int r = 0; r < 8; r++)
+            {
+                for (int c = 0; c < 8; c++)
+                {
+                    Image image = new Image();
+                    PieceImages[r, c] = image;
+                    PieceGrid.Children.Add(image);
+                }
+            }
         }
     }
 }

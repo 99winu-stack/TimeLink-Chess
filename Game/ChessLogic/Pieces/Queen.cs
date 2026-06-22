06621993c2a32b4a6984e081f7a56112e8e85210
Nguyen Le Hoang <nguyen.lehoang@student.htw-berlin.de>
2026-06-22 16:49:48 +0200

    Add Move Direction for rook, queen and bishop

--- Game/ChessLogic/Pieces/Queen.cs
+++ Game/ChessLogic/Pieces/Queen.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChessLogic
+﻿namespace ChessLogic
 {
     public class Queen : Piece
     {
         public override PieceType Type => PieceType.Queen;
         public override Player Color { get; }
+
+        private static readonly Direction[] dirs = new Direction[]
+        {
+            Direction.North, Direction.West, Direction.South, Direction.East,
+            Direction.NorthWest, Direction.NorthEast, Direction.SouthWest, Direction.SouthEast
+        };
 
         public Queen(Player color)
         {
@@ -21,6 +21,11 @@ namespace ChessLogic
             Queen copy = new Queen(Color);
             copy.HasMoved = HasMoved;
             return copy;
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            return MovePositionsInDirs(from, board, dirs).Select(to => new NormalMove(from, to));
         }
     }
 }

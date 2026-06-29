e8546660df2398d2becf7527caa97ca2095e3619
Nguyen Le Hoang <nguyen.lehoang@student.htw-berlin.de>
2026-06-29 11:26:20 +0200

    Implement pawn promotion menu

--- Game/GameUI/MainWindow.xaml.cs
+++ Game/GameUI/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace GameUI
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly Image[,] PieceImages = new Image[8, 8];
+        private readonly Image[,] pieceImages = new Image[8, 8];
         private readonly Rectangle[,] highlights = new Rectangle[8, 8];
         private readonly Dictionary<Position, Move> moveCache = new Dictionary<Position, Move>();
 
@@ -42,7 +42,7 @@ namespace GameUI
                 for (int c = 0; c < 8; c++)
                 {
                     Image image = new Image();
-                    PieceImages[r, c] = image;
+                    pieceImages[r, c] = image;
                     PieceGrid.Children.Add(image);
 
                     Rectangle hightlight = new Rectangle();
@@ -59,7 +59,7 @@ namespace GameUI
                 for (int c = 0; c < 8; c++)
                 {
                     Piece piece = board[r,c];
-                    PieceImages[r,c].Source = Images.GetImage(piece);
+                    pieceImages[r,c].Source = Images.GetImage(piece);
                 }
             }
         }
@@ -111,8 +111,33 @@ namespace GameUI
 
             if (moveCache.TryGetValue(pos, out Move move))
             {
-                HandleMove(move);
+                if(move.Type == MoveType.PawnPromotion)
+                {
+                    HandlePromotion(move.FromPos, move.ToPos);
+                }
+                else
+                {
+                    HandleMove(move);
+                }
+
+                    HandleMove(move);
             }
+        }
+
+        private void HandlePromotion(Position from, Position to)
+        {
+            pieceImages[to.Row, to.Column].Source = Images.GetImage(gameState.CurentPlayer, PieceType.Pawn);
+            pieceImages[from.Row, from.Column].Source = null;
+
+            PromotionMenu promMenu = new PromotionMenu(gameState.CurentPlayer);
+            MenuContainer.Content = promMenu;
+
+            promMenu.PieceSelected += type =>
+            {
+                MenuContainer.Content = null;
+                Move promMove = new PawnPromotion(from, to, type);
+                HandleMove(promMove);
+            };
         }
 
         private void HandleMove(Move move)
---
+++ Game/GameUI/PromotionMenu.xaml.cs
@@ -0,0 +1,40 @@
+﻿using ChessLogic;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace GameUI
+{
+    /// <summary>
+    /// Interaktionslogik für PromotionMenu.xaml
+    /// </summary>
+    public partial class PromotionMenu : UserControl
+    {
+        public event Action<PieceType> PieceSelected;
+        public PromotionMenu(Player player)
+        {
+            InitializeComponent();
+
+            QueenIMG.Source = Images.GetImage(player, PieceType.Queen);
+            BishopIMG.Source = Images.GetImage(player, PieceType.Bishop);
+            RookIMG.Source = Images.GetImage(player, PieceType.Rook);
+            KnightIMG.Source = Images.GetImage(player, PieceType.Knight);
+        }
+
+        private void QueenIMG_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            PieceSelected?.Invoke(PieceType.Queen);
+        }
+        private void BihopIMG_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            PieceSelected?.Invoke(PieceType.Bishop);
+        }
+        private void RookIMG_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            PieceSelected?.Invoke(PieceType.Rook);
+        }
+        private void KnightIMG_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            PieceSelected?.Invoke(PieceType.Knight);
+        }
+    }
+}

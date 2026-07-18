017452b0ac0187acc87aa84e303ed8784325a45f
99winu-stack <99winu@gmail.com>
2026-07-18 15:21:02 +0200

    Create fifty move rule

--- Game/ChessLogic/GameState.cs
+++ Game/ChessLogic/GameState.cs
@@ -12,6 +12,8 @@ namespace ChessLogic
         public Player CurentPlayer { get; private set; }
         public Result Result { get; private set; } = null;
 
+        private int noCaptureOrPawnMoves = 0;
+
         public GameState(Player player, Board board)
         {
             CurentPlayer = player;
@@ -33,8 +35,18 @@ namespace ChessLogic
         public void MakeMove(Move move)
         {
             Board.SetPawnSkipPosition(CurentPlayer, null);
-            move.Execute(Board);
-            CurentPlayer = CurentPlayer.Opponent();
+            bool captureOrPawn = move.Execute(Board);
+
+            if (captureOrPawn)
+            {
+                noCaptureOrPawnMoves = 0;
+            }
+            else
+            {
+                noCaptureOrPawnMoves++;
+            }
+
+            CurentPlayer = CurentPlayer.Opponent(); 
             CheckForGameOver();
         }
 
@@ -66,11 +78,21 @@ namespace ChessLogic
             {
                 Result = Result.Draw(EndReason.InsufficientMaterial);
             }
+            else if (FiftyMoveRule())
+            {
+                Result = Result.Draw(EndReason.FiftyMoveRule);
+            }
         }
 
         public bool IsGameOver()
         {
             return Result != null;
+        }
+
+        private bool FiftyMoveRule()
+        {
+            int fullMoves = noCaptureOrPawnMoves / 2;
+            return fullMoves >= 50;
         }
     }
 }
